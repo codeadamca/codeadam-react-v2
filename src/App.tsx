49d236954572f0c0ea702e594e8068ea9a174007
Adam <thomasadam83@hotmail.com>
2023-06-25 09:23:30 -0400

    Added sample full video

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
 import * as React from "react";
+
+import AppBar from "@mui/material/AppBar";
 import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
+import Toolbar from "@mui/material/Toolbar";
 import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
 import Link from "@mui/material/Link";
+
+import {SvgIcon} from "@mui/material";
+import {ReactComponent as Logo} from "./images/logo.svg";
+
 import ProTip from "./ProTip";
 
-import {AccessAlarm, ThreeDRotation, Delete} from "@mui/icons-material";
+import {
+  AccessAlarm,
+  PhotoCamera,
+  ThreeDRotation,
+  Delete,
+} from "@mui/icons-material";
 
 function Copyright() {
   return (
@@ -21,22 +34,97 @@ function Copyright() {
 
 export default function App() {
   return (
-    <Container maxWidth="sm">
-      <Box sx={{my: 4}}>
-        <AccessAlarm />
-        <ThreeDRotation />
-        <Delete />
-        <Delete color="primary" />
-        <Delete color="secondary" />
-        <Delete color="success" />
-        <Delete color="action" />
-        <Delete color="disabled" />
-        <Typography variant="h4" component="h1" gutterBottom>
-          Material UI Create React App example in TypeScript
-        </Typography>
-        <ProTip />
-        <Copyright />
+    <Container maxWidth={false}>
+      <AppBar position="absolute">
+        <Toolbar>
+          <SvgIcon sx={{mr: 1}} color="secondary">
+            <Logo />
+          </SvgIcon>
+          <Typography color="inherit" noWrap>
+            Teaching | Research and Publishings | Speaking Engagements |
+            Professional Development | About Me
+          </Typography>
+        </Toolbar>
+      </AppBar>
+
+      <Box
+        sx={{
+          backgroundImage: "url(https://source.unsplash.com/random?wallpapers)",
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          height: "100vh",
+          justifyContent: "center",
+        }}
+      >
+        <Box sx={{zIndex: 2}}>
+          <Typography variant="h1" color="secondary">
+            Adam Thomas
+          </Typography>
+          <Typography variant="h2" color="secondary">
+            I Teach Code!
+          </Typography>
+          <Typography color="secondary">
+            Self-taught full-stack developer.
+            <br />
+            Learning code and teaching code at Humber College, Toronto, Canada.
+          </Typography>
+        </Box>
+
+        <video
+          preload="auto"
+          autoPlay={true}
+          loop={true}
+          className="ca-fullscreen-background"
+          style={{
+            objectFit: "cover",
+            width: "100vw",
+            height: "100vh",
+            position: "absolute",
+            zIndex: 1,
+          }}
+        >
+          <source src="https://codeadam.ca/static/media/home-video.6057a6c8ab306b428d78.mp4" />
+        </video>
       </Box>
+
+      <Grid
+        container
+        component="main"
+        sx={{
+          backgroundImage: "url(https://source.unsplash.com/random?wallpapers)",
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          verticalAlign: "center",
+          height: "100vh",
+        }}
+      >
+        <Typography variant="h1" color="secondary">
+          Some Content
+        </Typography>
+      </Grid>
+
+      <Container>
+        <Box sx={{my: 4}}>
+          <AccessAlarm />
+          <ThreeDRotation />
+          <Delete />
+          <Delete color="primary" />
+          <Delete color="secondary" />
+          <Delete color="success" />
+          <Delete color="action" />
+          <Delete color="disabled" />
+          <Typography variant="h4" component="h1" gutterBottom>
+            Material UI Create React App example in TypeScript
+          </Typography>
+          <ProTip />
+          <Copyright />
+        </Box>
+      </Container>
     </Container>
   );
 }
--- src/theme.ts
+++ src/theme.ts
@@ -1,17 +1,29 @@
-import { createTheme } from '@mui/material/styles';
-import { red } from '@mui/material/colors';
+import {createTheme} from "@mui/material/styles";
+import {red} from "@mui/material/colors";
 
 // A custom theme for this app
 const theme = createTheme({
   palette: {
     primary: {
-      main: '#556cd6',
+      main: "#00ff00",
     },
     secondary: {
-      main: '#19857b',
+      main: "#ff0000",
     },
     error: {
       main: red.A400,
+    },
+  },
+  components: {
+    MuiContainer: {
+      styleOverrides: {
+        root: {
+          width: "100vw",
+          paddingLeft: "0 !important",
+          paddingRight: "0 !important",
+          border: "1px solid red",
+        },
+      },
     },
   },
 });
